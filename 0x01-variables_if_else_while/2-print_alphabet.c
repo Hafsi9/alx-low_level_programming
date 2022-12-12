8bd39b60398e898a7e6e304685c869c3f038a0a1
Your Name <you@example.com>
2022-12-12 07:14:41 -0800

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - Etry point
+ *
+ * Return: Alaways 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
